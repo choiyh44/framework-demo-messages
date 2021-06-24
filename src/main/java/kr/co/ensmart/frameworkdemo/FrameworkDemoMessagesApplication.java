d95085e069193c28321500f554cb52897a85d2f9
choiyh <choiyh44@gmail.com>
2021-06-24 13:41:14 +0900

    Initial commit

---
+++ src/main/java/kr/co/ensmart/frameworkdemo/FrameworkDemoMessagesApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ensmart.frameworkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkDemoMessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkDemoMessagesApplication.class, args);
+	}
+
+}
